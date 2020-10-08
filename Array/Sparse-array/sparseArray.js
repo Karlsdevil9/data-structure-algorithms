bbda2223e6f3c53af495196100e699e26c295565
vipuljain08 <vupuljain2001@gmail.com>
2020-10-08 09:51:27 +0530

    unstageg files

--- Array/Sparse-array/sparseArray.js
+++
@@ -1,9 +0,0 @@
-const sparseArray = (strings, queries) => {
-    let finalArray = []
-    for(let i=0; i<queries.length; i++) {
-        finalArray.push(strings.filter(st => st==queries[i]).length)
-    }
-    return finalArray
-}
-
-console.log(sparseArray(['ab', 'ab', 'abc'], ['ab', 'abc', 'bc']))
